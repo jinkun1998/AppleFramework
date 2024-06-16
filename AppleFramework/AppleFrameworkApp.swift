af0869f14326d36bceb702657abb01d74b3c7f8e
Quoc Thao <quocthao23061998.tg@gmail.com>
2024-06-16 16:51:17 +0700

    Initial Commit

---
+++ AppleFramework/AppleFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkApp.swift
+//  AppleFramework
+//
+//  Created by Jin on 16/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFrameworkTests/AppleFrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkTests.swift
+//  AppleFrameworkTests
+//
+//  Created by Jin on 16/6/24.
+//
+
+import Testing
+@testable import AppleFramework
+
+struct AppleFrameworkTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
